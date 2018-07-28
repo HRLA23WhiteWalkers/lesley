a7c7f1c3ccf6196bd2cb5cc8f8ea8f9f9575fa6c
LesleyPark <lesparkley@gmail.com>
2018-07-27 17:34:02 -0700

    Try incorporating Lightbox Component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactModal from 'react-modal';
+import Lightbox from 'react-images'
 import ScrollLock from 'react-scrolllock';
 import axios from 'axios';
 import Gallery from './Gallery.jsx';
@@ -9,11 +10,12 @@ class Header extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showModal: false,
+      // showModal: false,
+      lightboxIsOpen: false,
       photos: []
     }
-    this.handleOpenModal = this.handleOpenModal.bind(this);
-    this.handleCloseModal = this.handleCloseModal.bind(this);
+    // this.handleOpenModal = this.handleOpenModal.bind(this);
+    // this.handleCloseModal = this.handleCloseModal.bind(this);
   }
 
   componentDidMount() {
@@ -21,12 +23,28 @@ class Header extends React.Component {
     console.log(this.state.photos);
   }
 
-  handleOpenModal() {
-    this.setState({ showModal: true });
-  }
+  // handleOpenModal() {
+  //   this.setState({ showModal: true });
+  // }
   
-  handleCloseModal() {
-    this.setState({ showModal: false });
+  // handleCloseModal() {
+  //   this.setState({ showModal: false });
+  // }
+
+  handleClick() {
+    this.setState({ightboxIsOpen: true})
+  }
+
+  closeLightbox() {
+    this.setState({lightboxIsOpen: false})
+  }
+
+  goToPrevious() {
+
+  }
+
+  goToNext() {
+
   }
 
   fetchPhotos() {
@@ -41,14 +59,18 @@ class Header extends React.Component {
   render() {
     return (
       <div className={styles.container}>
-        <img onClick={this.handleOpenModal} 
+        <img 
+          // onClick={this.handleOpenModal} 
+          onClick={this.handleClick()} 
           className={styles.cover} 
           src="https://s3-us-west-1.amazonaws.com/hackreactorlp/assets/10.jpg" 
         />
-        <button onClick={this.handleOpenModal} className={styles.pbtn}>View Photos</button>
+        {/* <button onClick={this.handleOpenModal} className={styles.pbtn}>View Photos</button> */}
+        <button onClick={this.handleClick} className={styles.pbtn}>View Photos</button>
         <button className={styles.sbtn}><img className={styles.saveicon} src="https://s3-us-west-1.amazonaws.com/hackreactorlp/save.png" /> Save</button>
         <button className={styles.sbtn}><img className={styles.shareicon} src="https://s3-us-west-1.amazonaws.com/hackreactorlp/share.png" /> Share</button>
-        <div className={styles.spacer}>
+        {/* {this.state.lightboxIsOpen ? <Gallery isOpen={this.state.lightboxIsOpen} /> : null} */}
+        {/* <div className={styles.spacer}>
           <ReactModal 
             isOpen={this.state.showModal}
             className={styles.Modal}
@@ -57,8 +79,18 @@ class Header extends React.Component {
             <Gallery />
             <button className={styles.close} onClick={this.handleCloseModal}>Close Modal</button>
           </ReactModal>
-        </div>
-        {this.state.showModal ? <ScrollLock /> : null}
+        </div> */}
+        {/* {this.state.showModal ? <ScrollLock /> : null} */}
+        {this.state.lightboxIsOpen ? 
+          <Lightbox
+            images={[{ src: 'https://s3-us-west-1.amazonaws.com/hackreactorlp/assets/10.jpg' }, 
+              { src: 'https://s3-us-west-1.amazonaws.com/hackreactorlp/assets/10.jpg' }]}
+            isOpen={this.state.lightboxIsOpen}
+            // onClickPrev={this.gotoPrevious}
+            // onClickNext={this.gotoNext}
+            onClose={this.closeLightbox}
+          />
+          : null}
       </div>
     )
   }
